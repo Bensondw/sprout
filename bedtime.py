0939ec40f909edb2352841f4beee720449e5225e
Benson Weeks <bensonweeks@gmail.com>
2018-04-10 10:20:32 -0600

    added blank scripts for bedtime and muting music when user listens to mic

---
+++ bedtime.py
@@ -0,0 +1,31 @@
+#! /usr/bin/python 
+
+# import script
+# import script
+
+
+# start music
+
+
+# start lightshow
+
+
+# start motion sensor detection
+
+
+# react to motion
+
+
+	# turn off lightshow
+
+	# turn off music - mute it
+
+		# wait until there has been 1 minute without motion 
+
+		# start up show again
+
+
+# end nighttime / bedtime show
+
+
+
---
+++ micOn.py
@@ -0,0 +1,5 @@
+#! /usr/bin/python
+
+# mute music whether any is playing or not
+
+# either start the ice cast server here, or let that have been done earlier when the website first loaded. 
